dfc0c51b43a2bf7c896eabdeec8c244f69c95a43
jeleo <jeleo@qq.com>
2016-07-18 09:53:17 +0800

    code staging, incomplete

--- open/src/main/webapp/assets/self/js/controllers/weixin/setup.js
+++ open/src/main/webapp/assets/self/js/controllers/weixin/setup.js
@@ -1,18 +1,14 @@
 app.controller('WeixinSetupController', ['$scope', '$http', '$state', '$location', function ($scope, $http, $state, $location) {
-    var setupTypeIsAdd = new RegExp("add" + "$").test($location.url());
-    if (setupTypeIsAdd) {
-        $scope.setupTypeDisplay = "添加";
-    } else {
-        $scope.setupTypeDisplay = "修改";
-    }
     $scope.back = function () {
         $state.go("app.weixin.list");
     };
-    // 优先获取微信公众号枚举类型
-    $http.get(api("/constants/weixin/accountType.json"))
-        .success(function (data) {
-            $scope.accountTypes = data;
-            if (setupTypeIsAdd) {
+    var setupTypeIsAdd = new RegExp("add" + "$").test($location.url());
+    if (setupTypeIsAdd) {
+        $scope.setupTypeDisplay = "添加";
+        // 优先获取微信公众号枚举类型
+        $http.get(api("/constants/weixin/accountType.json"))
+            .success(function (data) {
+                $scope.accountTypes = data;
                 $scope.saveSetup = function () {
                     $http.post(api("/weixin/add.json"), $scope.setup)
                         .success(function (data) {
@@ -24,14 +20,16 @@ app.controller('WeixinSetupController', ['$scope', '$http', '$state', '$location
                         })
                     ;
                 }
-            } else {
-                var id = $location.search()['id'];
-                $http.get(api("/weixin/get.json?id=" + id))
-                    .success(function (data) {
-                        $scope.setup = data;
-                    });
-            }
-        });
-    $scope.accountType = "";
+            });
+        $scope.accountType = "";
+    } else {
+        $scope.setupTypeDisplay = "修改";
+        var id = $location.search()['id'];
+        $http.get(api("/weixin/get.json?id=" + id))
+            .success(function (data) {
+                $scope.setup = data;
+            });
+    }
+
 
 }]);
